1da4abe61aa0d5e13dc548a82f92a194a73ea599
Samuel Håkans Persson <samuhaka100@kau.se>
2025-09-14 15:47:34 +0200

    minimum required information on every page

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("assets");
 
   return {
     dir: {
